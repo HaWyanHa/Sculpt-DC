d6b1d2a728d88b0f50b376ea3b87a74b42d99975
footclan2k2 <ryan@hugsbyha.com>
2016-06-09 12:30:47 -0400

    added cycling classes

---
+++ main.js
@@ -0,0 +1,17 @@
+// (function () {
+// 	"use strict";
+// 	console.log("inside IFE");
+
+// 	$("#new-hover").hover(function(){
+// 		$("#test").show("400");
+// 		}, function() {
+// 			$("#test").hide("900");
+// 	});
+
+// 	$("ul").hover(function(event){
+// 		event.stopPropagation();
+// 	}, function(){
+// 		$("ul").hide();
+// 	})
+
+// })();
