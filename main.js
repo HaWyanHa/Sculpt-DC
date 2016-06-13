86301bde2b782f88d85e243cfe88c2198ac52cd1
footclan2k2 <ryan@hugsbyha.com>
2016-06-13 11:10:53 -0400

    slide toggle works

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
-// (function () {
-// 	"use strict";
-// 	console.log("inside IFE");
+(function () {
+	"use strict";
+	console.log("inside IFE");
 
-// 	$("#new-hover").hover(function(){
-// 		$("#test").show("400");
-// 		}, function() {
-// 			$("#test").hide("900");
-// 	});
+	// $("#new-hover").hover(function(){
+	// 	$("#test").show("400");
+	// 	}, function() {
+	// 		$("#test").hide("900");
+	// });
 
-// 	$("ul").hover(function(event){
-// 		event.stopPropagation();
-// 	}, function(){
-// 		$("ul").hide();
-// 	})
+	// $("ul").hover(function(event){
+	// 	event.stopPropagation();
+	// }, function(){
+	// 	$("ul").hide();
+	// })
 
-// })();
+	$("#test").hide();
+
+	$("#new-hover").mouseover(function(){
+		$("#test").slideDown("slow");
+	});
+
+	$("#wrapper").mouseleave(function(){
+		$("#test").slideUp("slow");
+	});
+})();
