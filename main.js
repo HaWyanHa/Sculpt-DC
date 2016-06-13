77561b7e696e9a33671fd09910af56dad11116a2
footclan2k2 <ryan@hugsbyha.com>
2016-06-13 11:34:50 -0400

    all dropdowns for new students working

--- main.js
+++ main.js
@@ -14,13 +14,36 @@
 	// 	$("ul").hide();
 	// })
 
-	$("#test").hide();
+	$("#dropdown1").hide();
+	$("#dropdown2").hide();
+	$("#dropdown3").hide();
+	$("#dropdown4").hide();
 
-	$("#new-hover").mouseover(function(){
-		$("#test").slideDown("slow");
+	$("#new-hover1").mouseover(function(){
+		$("#dropdown1").slideDown("slow");
+	});
+	$("#new-hover2").mouseover(function(){
+		$("#dropdown2").slideDown("slow");
+	});
+	$("#new-hover3").mouseover(function(){
+		$("#dropdown3").slideDown("slow");
+	});
+	$("#new-hover4").mouseover(function(){
+		$("#dropdown4").slideDown("slow");
 	});
 
-	$("#wrapper").mouseleave(function(){
-		$("#test").slideUp("slow");
+	$("#wrapper1").mouseleave(function(){
+		$("#dropdown1").slideUp("fast");
 	});
+	$("#wrapper2").mouseleave(function(){
+		$("#dropdown2").slideUp("fast");
+	});
+	$("#wrapper3").mouseleave(function(){
+		$("#dropdown3").slideUp("fast");
+	});
+	$("#wrapper4").mouseleave(function(){
+		$("#dropdown4").slideUp("fast");
+	});
+
+	
 })();
