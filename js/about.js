3509c6b8e76f28877e58dfe856c4d7f1202c6e03
footclan2k2 <ryan@hugsbyha.com>
2016-07-18 13:25:36 -0400

    javascript working for faq will clean it up next

--- js/about.js
+++ js/about.js
@@ -4,10 +4,15 @@
 	ns.about = {};
 
 	ns.about.load = function showAbout() {
-		
+
 		$("#home").hide();
 		$("#about").show();
 
 	};
+
+	$("#faqhead1").click(function(){
+	 	$("#faqbody1").toggle("fast");
+	});
+
 	window.sculpt = ns;
 })(window.sculpt || {});
