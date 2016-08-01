9d53cc0e8c0e21747e8b41782c1790316876029f
footclan2k2 <ryan@hugsbyha.com>
2016-08-01 17:51:42 -0400

    styled the navbar and fixed the footer

--- js/about.js
+++ js/about.js
@@ -18,9 +18,50 @@
 		$(this).next("p").toggle("fast");
 	});
 
+
+
 	$("#faq").click(function(){
 		$(".about-head").hide();
+		$(".team").hide();
+		$(".classes").hide();
 		$(".faq").show();
+		$("#faq").addClass("active-subnav");
+		$("#body").removeClass("active-subnav");
+		$("#classes").removeClass("active-subnav");
+		$("#team").removeClass("active-subnav");
+	})
+
+	$("#body").click(function(){
+		$(".faq").hide();
+		$(".team").hide();
+		$(".classes").hide();
+		$(".about-head").show();
+		$("#faq").removeClass("active-subnav");
+		$("#body").addClass("active-subnav");
+		$("#classes").removeClass("active-subnav");
+		$("#team").removeClass("active-subnav");
+	})
+
+	$("#team").click(function(){
+		$(".faq").hide();
+		$(".about-head").hide();
+		$(".classes").hide();
+		$(".team").show();
+		$("#faq").removeClass("active-subnav");
+		$("#body").removeClass("active-subnav");
+		$("#classes").removeClass("active-subnav");
+		$("#team").addClass("active-subnav");
+	})
+
+	$("#classes").click(function(){
+		$(".faq").hide();
+		$(".about-head").hide();
+		$(".classes").show();
+		$(".team").hide();
+		$("#faq").removeClass("active-subnav");
+		$("#body").removeClass("active-subnav");
+		$("#classes").addClass("active-subnav");
+		$("#team").removeClass("active-subnav");
 	})
 
 	window.sculpt = ns;
