36e66699b76e1dd0186a46378d0b6a12c7baa4b5
footclan2k2 <ryan@hugsbyha.com>
2016-08-04 15:34:53 -0400

    javascript nav working for pricing

--- js/pricing.js
+++ js/pricing.js
@@ -18,6 +18,33 @@
 		$(this).next("p").toggle("fast");
 	});
 
+	$("#cycle-price-link").click(function(){
+		$("#cycle-price").show();
+		$("#cycle-price-link").addClass("active-subnav");
+		$("#yoga-price").hide();
+		$("#yoga-price-link").removeClass("active-subnav");
+		$("#ride-price").hide();
+		$("#ride-price-link").removeClass("active-subnav");
+	});
+
+	$("#yoga-price-link").click(function(){
+		$("#yoga-price").show();
+		$("#yoga-price-link").addClass("active-subnav");
+		$("#cycle-price").hide();
+		$("#cycle-price-link").removeClass("active-subnav");
+		$("#ride-price").hide();
+		$("#ride-price-link").removeClass("active-subnav");
+	});
+
+	$("#ride-price-link").click(function(){
+		$("#ride-price").show();
+		$("#ride-price-link").addClass("active-subnav");
+		$("#yoga-price").hide();
+		$("#yoga-price-link").removeClass("active-subnav");
+		$("#cycle-price").hide();
+		$("#cycle-price-link").removeClass("active-subnav");
+	});
+
 
 	window.sculpt = ns;
 })(window.sculpt || {});
